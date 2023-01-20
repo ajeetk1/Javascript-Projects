a73284c8d2d4e0fddcb03ee351d48e58ccd9d476
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-01-20 18:54:12 +0530

    Landing Page Update

--- Single Page Website/app.js
+++ Single Page Website/app.js
@@ -1,3 +1,7 @@
 const imageEl = document.getElementById("image-el")
 //console.log(imageEl);
 
+window.addEventListener('scroll', () => {
+    const scrollPercent = (window.scrollX / window.innerHeight) * 100;
+    imageEl.style.transform = `rotate(${scrollPercent}deg)`;
+  });
