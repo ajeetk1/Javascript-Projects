4929fb1373866280d538462f6bdea76a1e57fb80
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-02-02 17:23:46 +0530

    Updates

---
+++ Expanding Cards/app.js
@@ -0,0 +1,29 @@
+//Fetch from code all. query selector CSS classes
+
+const panels = document.querySelectorAll(".panel");
+
+// On Click functionity all
+// For Each Loop - Click that expands
+
+ panels.forEach(panel=>{
+ panel.addEventListener("click", () =>{
+removeActiveClasses()
+panel.classList.add('active');
+})
+
+ })
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+
+
+
+ 
+
+
+
+
+
