e4b414161ae6a76cb5fbaf0dbc741097c0472b1d
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-02-07 19:50:53 +0530

    Updates

--- Expanding Cards/app.js
+++ Expanding Cards/app.js
@@ -5,25 +5,15 @@ const panels = document.querySelectorAll(".panel");
 // On Click functionity all
 // For Each Loop - Click that expands
 
- panels.forEach(panel=>{
- panel.addEventListener("click", () =>{
-removeActiveClasses()
-panel.classList.add('active');
+
+panels.forEach(panel=>{
+panel.addEventListener("click",()=>{
+    removeActiveClasses()
+panel.classList.add('active')
 })
-
- })
-
+})
 function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('active')
     })
 }
-
-
-
- 
-
-
-
-
-
