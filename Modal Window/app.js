56adf42bc5f0f1005f78627f64b355200ccfbe75
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-03-07 16:58:02 +0530

    Javascript Project

--- Modal Window/app.js
+++ Modal Window/app.js
@@ -6,25 +6,44 @@ const modal = document.querySelectorAll('.btn-modal');
 console.log(modal);
 
 //selecting the Cross Sign 
-
 const close = document.querySelector('.close-modal');
 console.log(close);
+
 // Selecting overlay
 const background= document.querySelector('.background')
 console.log(background);
+
 //Select Hidden Class
 const hidden = document.querySelector('.hidden');
 console.log(hidden);
+
+//Select pop up class
+const modalOpen = document.querySelector('.modal');
+console.log(modalOpen);
 // Functionlity is Click on Button 
 
-for(let i=0;i<modal.length;i++){
+for(let i=0;i< modal.length;i++){
 modal[i].addEventListener('click', function(){
 console.log("Button Clicked");
 
-
+modalOpen.classList.remove('hidden');
+background.classList.remove('hidden');
 });
 
+// click on close
+close.addEventListener('click', function(){
+console.log("clicked");
+modalOpen.classList.add('hidden');
+background.classList.add('hidden');
+});
 
+// overlay 
+
+background.addEventListener('click', function() {
+console.log("Background Again Clicked");
+modalOpen.classList.add('hidden');
+background.classList.add('hidden');
+})
 
 }
 
