fab86468e3cb44f298fcf43f8c5cb3718aba97d6
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-03-30 23:30:47 +0530

    Javascipt-Projects

--- Counter Javascript/script.js
+++ Counter Javascript/script.js
@@ -1,5 +1,5 @@
 //My Method
-
+/*
 let number =0;
 
 const root = document.querySelector(".number");
@@ -30,5 +30,43 @@ number= number+1;
 document.querySelector('.number').textContent=number;
 document.querySelector('.number').style.color = "green";
 })
+*/
+
+// set initial number 
+let number = 0;
+
+// Select buttons and 
+let root = document.querySelector('.number');
+console.log(root)
+let Button = document.querySelectorAll('.button');
+console.log(Button)
+
+// For Each loop we have to use 
+
+Button.forEach(function(item) {
+item.addEventListener('click', function(e) {
+
+e.currentTarget.classList; // Selecting all Button Classes
+console.log(e.currentTarget.classList)
+
+const styles = e.currentTarget.classList
+
+if(styles.contains('decrease')) {
+    number--;   
+} else if (styles.contains('increase')){
+    number++;
+} else {
+    number=0;
+}
+if (number<0){
+    root.style.color = "red";
+    } if(number>0){
+        root.style.color = "green";
+    } if (number===0)
+        root.style.color = "orange";
+    
+root.textContent=number;
 
 
+});
+});
