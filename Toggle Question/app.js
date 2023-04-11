dde6eca6b1b83af3062642b1e65e1d788dd5daf3
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-04-11 16:54:39 +0530

    Javascript-Projects

--- Toggle Question/app.js
+++ Toggle Question/app.js
@@ -1,11 +1,22 @@
 const btn = document.querySelector('.btn');
+console.log(btn);
 const text = document.querySelector('.text');
+console.log(text)
 const first = document.querySelector('.first');
+console.log(first)
+const minus = document.querySelector('.minus');
+console.log(minus);
+const second = document.querySelector('.second');
+console.log(second)
 
-btn.addEventListener('click' , function() {
-console.log("Button Clicked")
-first.style.height ="300px";
-text.style.display = 'block';
+btn.addEventListener('click', function(){
+console.log("button clicked")
+text.style.display = "block";
+minus.style.display='block';
 });
 
- 
+minus.addEventListener('click', function() {
+text.style.display ='none';
+minus.style.display ="none";
+})
+
