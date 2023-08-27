5ea1e6b8bc5e004fbad22b5f4428b8d0b550e553
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2023-08-27 17:00:42 +0530

    Javascript Projects

--- Age Calculator/main.js
+++ Age Calculator/main.js
@@ -12,12 +12,33 @@ const birthdayValue = number.value;
 if (birthdayValue == ""){
   console.log("Ask user to enter Birth Date")
 } else {
-// Birthday Date is Entered 
-
-
+// Birthday Date is Entered then fetch value of date
+const age  = getAge(birthdayValue);
+ageResult.innerText= `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
 }
 
 }
+
+// We have to calculate the age curret year - Birthday Date
+
+function getAge(birthdayValue){
+
+
+     const currentDate = new Date();  // new Date Function Javascript
+    const birthdayDate = new Date(birthdayValue);  //Fetch Birthday Date 
+    let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+    const month = currentDate.getMonth() - birthdayDate.getMonth();
+  
+    if (
+      month < 0 ||
+      (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+    ) {
+      age--;
+    }
+  
+    return age;
+  }
+  button.addEventListener("click", calculateAge);
 
 
 
