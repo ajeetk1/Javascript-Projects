b3315cc2fa2d71d33bf230c24d97284d09ecff7e
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2024-02-06 17:15:04 +0530

    Javascript Projects

---
+++ Construction /app.js
@@ -0,0 +1,17 @@
+// const accordion = document.getElementsByClassName('faq-box');
+
+
+// for(i=0; i<accordion.length; i++){
+//  accordion[i].addEventListener('click' ,function (){
+//  this.classList.toggle('active');
+//  })
+
+// }
+
+
+function toggleMenu() {
+    var nav = document.getElementById("myNav");
+    console.log(nav);
+    nav.classList.toggle("show");
+}
+
--- Construction /script.js
+++ Construction /script.js
@@ -56,20 +56,5 @@ $(document).ready(function () {
     });
 });
 
-    
-function toggleAnswer(answerId) {
-    var answer = document.getElementById(answerId);
-    var question = answer.previousElementSibling;
+/*-----------FAQ -----------*/
 
-    if (answer.style.maxHeight === "0px" || answer.style.maxHeight === "") {
-        // Expand answer
-        answer.style.maxHeight = answer.scrollHeight + "px";
-        question.classList.add("expanded");
-        document.querySelector('.toggle-icon', question).innerHTML = '×';
-    } else {
-        // Collapse answer
-        answer.style.maxHeight = "0";
-        question.classList.remove("expanded");
-        document.querySelector('.toggle-icon', question).innerHTML = '+';
-    }
-}
