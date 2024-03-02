fa5b2088541e80968f4090bcbbee564a8d3d82a4
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2024-03-02 22:55:55 +0530

    Javascript Projects

---
+++ Pomdoro Timer/index.js
@@ -0,0 +1,12 @@
+// select all Buttons 
+//
+//
+
+const start = document.getElementsByClassName(".start");
+console.log("start");
+
+const stop =document.getElementsByClassName(".stop");
+console.log(".stop");
+
+const reset =document.getElementsByClassName(".reset");
+console.log(".reset");
